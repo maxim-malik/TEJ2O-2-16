2c91e505d6772d2c5bdbf6ddb5f39b3f503a3e25
Maxim Malik <maxim.malik@stu.ocsb.ca>
2025-12-15 22:59:22 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maxim Malik
+ * Created on: Dec 2025
+ * This program uses radios and an ultrasonic sensor to send distance to another microbit
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(23)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // checking distance with sonar
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    // if the distance is less than 10 cm, send alert (!) to microbit, if distance is more than 10 cm send star (*)
+    if (distanceToObject <= 10) {
+        radio.sendString('!')
+    } else {
+        radio.sendString('*')
+    }
+})
+
+// receiving message
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
